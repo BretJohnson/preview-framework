dbe0fdbbb78603729f9d990ca88f8202b0e20d72
Bret Johnson <bret.johnson@microsoft.com>
2023-03-07 14:18:40 -0500

    Initial commit

---
+++ samples/maui/WeatherTwentyOne/src/WeatherTwentyOne/Views/CurrentWidget.xaml.cs
@@ -0,0 +1,17 @@
+﻿using ExampleBook;
+using WeatherTwentyOne.ViewModels;
+
+namespace WeatherTwentyOne.Views;
+
+public partial class CurrentWidget
+{
+    public CurrentWidget()
+    {
+        InitializeComponent();
+    }
+
+#if DEBUG
+    [Example("My Example")]
+    public static CurrentWidget Example() => new CurrentWidget(new HomeViewModel());
+#endif
+}
