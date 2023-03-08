5ebd399a4db5ea5060f377bb4311d9d130edf54e
Bret Johnson <bret.johnson@microsoft.com>
2023-03-08 12:43:16 -0500

    Move SandboxApp to samples/maui directory

---
+++ samples/maui/SandboxApp/TestPage1.xaml.cs
@@ -0,0 +1,16 @@
+using ExampleBook;
+
+namespace SandboxApp;
+
+public partial class TestPage1 : ContentPage
+{
+	public TestPage1()
+	{
+		InitializeComponent();
+	}
+
+#if DEBUG
+    [Example("MyCategory/Example With Custom Params")]
+    public static TestPage1 Example1() => new TestPage1();
+#endif
+}
