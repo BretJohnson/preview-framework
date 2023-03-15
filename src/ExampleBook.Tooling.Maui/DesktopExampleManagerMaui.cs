161778814764c90fec3e68b5e7b6893ed8b833d4
Bret Johnson <bret.johnson@microsoft.com>
2023-03-15 13:51:23 -0400

    One more update

--- src/ExampleBook.Tooling.Maui/DesktopExampleManagerMaui.cs
+++ src/ExampleBook.Tooling.Maui/DesktopExampleManagerMaui.cs
@@ -1,11 +1,51 @@
 ﻿using ExampleBook.Tooling.Maui.ViewModels;
 using ExampleBook.Tooling.Maui.Views;
+using System.Diagnostics;
 
 namespace ExampleBook.Tooling.Maui;
 
-public static class ExamplesManagerDesktopMaui
+public static class ExamplesManagerMaui
 {
     private static Window _window = null;
+
+    public static bool LaunchInExamplesMode(Application app, string? environmentVariable = null)
+    {
+        if (! ShouldLaunchInExamplesMode(environmentVariable))
+            return false;
+
+        if (true /* isDesktop */)
+        {
+            var desktopMainPage = new DesktopMainPage();
+            desktopMainPage.BindingContext = new UIExamplesManagerDesktopViewModel(CurrentAppUIExamplesManager.Instance);
+
+            app.MainPage = desktopMainPage;
+            return true;
+        }
+    }
+
+    public static bool ShouldLaunchInExamplesMode(string? environmentVariable = null)
+    {
+        if (environmentVariable != null)
+        {
+            string? value = Environment.GetEnvironmentVariable(environmentVariable);
+            if (value != null && !string.Equals(value, "0") && !string.Equals(value, "false", StringComparison.OrdinalIgnoreCase))
+                return true;
+        }
+
+        string[] args = Environment.GetCommandLineArgs();
+
+        int count = args.Length;
+        for (int i = 0; i < count; i++)
+        {
+            string arg = args[i];
+            if (string.Equals(arg, "-examplesmode", StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     public static void ShowWindow()
     {
