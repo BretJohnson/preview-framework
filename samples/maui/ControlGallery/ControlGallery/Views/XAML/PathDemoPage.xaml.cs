622d978444617931779ab15d99e87dd861d91124
Bret Johnson <bret.johnson@microsoft.com>
2023-09-04 14:45:40 -0400

    Fix to use right Path class

--- samples/maui/ControlGallery/ControlGallery/Views/XAML/PathDemoPage.xaml.cs
+++ samples/maui/ControlGallery/ControlGallery/Views/XAML/PathDemoPage.xaml.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.Maui.Controls;
-
-namespace ControlGallery.Views.XAML
+﻿namespace ControlGallery.Views.XAML
 {
-    [UIExample(typeof(Path))]
+    [UIExample(typeof(Microsoft.Maui.Controls.Shapes.Path))]
     public partial class PathDemoPage : ContentPage
     {
         public PathDemoPage()
